33e7645400f36ed958b29f1ba1f87a219ae618d5
vgianluca96 <gianluca.vallese@hotmail.com>
2023-09-08 14:56:36 +0200

    creato array posts
    per ora messo solo un oggetto post con tutte le proprietà

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
 
+/*
+Milestone 1
+Creiamo il nostro array di oggetti che rappresentano ciascun post.
+Ogni post dovrà avere le informazioni necessarie per stampare la relativa card:
+id del post, numero progressivo da 1 a n
+nome autore,
+foto autore,
+data in formato americano (mm-gg-yyyy),
+testo del post,
+immagine (non tutti i post devono avere una immagine),
+numero di likes.
+Non è necessario creare date casuali
+Per le immagini va bene utilizzare qualsiasi servizio di placeholder ad es. Unsplash (https://unsplash.it/300/300?image=<id>)
+Milestone 2
+Prendendo come riferimento il layout di esempio presente nell'html, stampiamo i post del nostro feed.
+Milestone 3
+Se clicchiamo sul tasto "Mi Piace" cambiamo il colore al testo del bottone e incrementiamo il counter dei likes relativo. Salviamo in un secondo array gli id dei post ai quali abbiamo messo il like.
+*/
 
-console.log('ciao')
+
+let posts = [
+    {
+        id: 1,
+        name: 'Gianluca',
+        profilePic: 'https://picsum.photos/100/100?random=1',
+        date: new Date(),
+        text: 'Questo è il testo del post',
+        image: 'https://picsum.photos/100/100?random=2',
+        likes: 80
+    }
+
+];
+
+
+console.log(posts);
